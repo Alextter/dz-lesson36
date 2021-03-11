c6ff6dd9738ce8289801c3b7044f9c1f605a1660
Alextter <74066029+Alextter@users.noreply.github.com>
2021-03-11 17:06:58 +0200

    Add files via upload

---
+++ Новая папка (3)/main.js
@@ -0,0 +1,16 @@
+// let a =Number (prompt(" Введите число а"))
+// b = Number (prompt("Введите число б"))
+// if( a <= 1  && b >= 3){
+//     alert( a + b)
+// }
+// else {
+//     alert(a-b)
+// }
+let c =Number (prompt(" Введите число с"))
+d = Number (prompt("Введите число d"))
+if( c > 2  && c<11 || d >= 6 && d<14){
+    alert("верно")
+}
+else {
+    alert("Не верно")
+}
