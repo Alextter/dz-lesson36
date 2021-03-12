f78e68c1dc84a32eed790ea0d01cd8c3de642fe2
Alextter <74066029+Alextter@users.noreply.github.com>
2021-03-12 15:20:45 +0200

    Add files via upload

---
+++ xcv/main.js
@@ -0,0 +1,37 @@
+// let a =Number (prompt(" Введите число а"))
+// b = Number (prompt("Введите число б"))
+// if( a <= 1  && b >= 3){
+//     alert( a + b)
+// }
+// else {
+//     alert(a-b)
+// }
+
+
+// let c =Number (prompt(" Введите число с"))
+// d = Number (prompt("Введите число d"))
+// if( c > 2  && c<11 || d >= 6 && d<14){
+//     alert("верно")
+// }
+// else {
+//     alert("Не верно")
+// }
+
+
+// let a =Number (prompt(" Введите число а"))
+// if ( a>0 && a<5){
+//     alert("Verno")
+// }
+// else{
+//     alert("Ne verno")
+// }
+
+// let a =Number (prompt(" Введите число а"))
+// let b = 7
+// let c = 10
+// if ( a==0 || a==2){
+//     alert( a+b)
+// }
+// else{
+// alert(a/c )
+// }
